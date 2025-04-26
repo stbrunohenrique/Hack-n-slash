95f9653b0865de06d736a6d64691d7c3b072096e
Bruno Henrique Gomes Soares <brunohgs06@gmail.com>
2025-04-26 20:44:11 -0300

    Novas adições!
    - Novo design de nível,
    - Criação da entidade Enemy,
    - Criação de plataformas que movimentam em uma dada direção.

--- assets/stuffs/cherry.gd
+++ assets/stuffs/cherry.gd
@@ -2,8 +2,9 @@ extends AnimatedSprite2D
 
 signal get_cherry(amount: int)
 
-func _physics_process(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	play("default")
+	await animation_finished
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
